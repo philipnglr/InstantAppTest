f923ccca8e825a77e46b88fcb03e6c0ae27b9eed
Philip Engler <philip.engler@gmx.de>
2020-12-06 16:59:28 +0100

    Initial Commit
    Uploaded all App files

---
+++ installed/src/main/java/com/example/instantapptest/InstalledActivity.java
@@ -0,0 +1,14 @@
+package com.example.instantapptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstalledActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_installed);
+    }
+}
